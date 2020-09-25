4d208b1b2e0aff6eb0044dd4fdf743f04259aa5a
Shunsuke Takagi (MacBook Pro) <takagishunsuke1129@gmail.com>
2020-09-25 20:00:11 +0900

    Initial Commit

---
+++ SwiftUIAppStore/SwiftUIAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppStoreApp.swift
+//  SwiftUIAppStore
+//
+//  Created by Shunsuke Takagi on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
